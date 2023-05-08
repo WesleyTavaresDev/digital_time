fb761c9f69c835f490f932cd4ea33334eab0f008
WesleyTavaresDev <wesleytaveresdev@outlook.com>
2023-05-07 21:18:32 -0300

    window creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use chrono::prelude::*;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder().application_id("org.time").build();
+
+    app.connect_activate(setup);
+    app.run();
+}
+
+fn setup(application: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .title("time")
+        .default_width(180)
+        .default_height(240)
+        .build();
+
+    window.present();
 }
